66483398408e01f853b40818343f41a124fd4192
Nathan M. Premo <nmpremo1@csis.svsu.edu>
2019-03-13 13:24:38 -0400

    updated

--- customers.class.php
+++ customers.class.php
@@ -292,15 +292,15 @@ class Customer {
         echo "
             </head>
             <body>
-                <a href='https://github.com/Grimmjow91/Crud_OO' target='_blank'>Github</a><br />
-				<a href='http://csis.svsu.edu/~nmpremo1/cis355/crud_oo_diagram.vsdx' target='_blank'>Diagram</a><br />
+                <a href='https://github.com/cis355/PhpProject1' target='_blank'>Github</a><br />
                 <div class='container'>
                     <p class='row'>
                         <h3>$this->title" . "s" . "</h3>
                     </p>
                     <p>
                         <a href='$this->tableName.php?fun=display_create_form' class='btn btn-success'>Create</a>
-                    </p>
+						<a href='logout.php' class='btn btn-warning'>Logout</a> 
+					</p>
                     <div class='row'>
                         <table class='table table-striped table-bordered'>
                             <thead>
